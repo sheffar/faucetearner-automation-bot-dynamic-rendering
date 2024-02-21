2f20eacd4119cf6a8b96c76bbc4d5d485ae8d35a
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-02-21 12:34:41 +0100

    parse url params

--- index.js
+++ index.js
@@ -5,11 +5,16 @@ const app = express()
 
 const PORT = process.env.PORT || 4000
 
-app.get("/", (req,res) => {
+app.get("/", (req, res) => {
   console.log('Ping');
   const { uname, pswd } = req.query;
-  res.send('Intialising bot for uname:' + uname + ' pswd:' + pswd)
-  mineLogic(res,uname,pswd);
+  if (uname == undefined || pswd == undefined) {
+    res.send('Params error! Kill the script');
+    console.log('Params error! Kill the script');
+  } else {
+    res.send('Intialising bot for uname:' + uname + ' pswd:' + pswd)
+    mineLogic(res, uname, pswd);
+  }
 })
 
 app.listen(PORT, () => {
